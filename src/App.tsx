b7c5d5f77f6c1aaba09551ae90ea2b0a549cbb21
Leandro Reis <lleandrofr@gmail.com>
2021-10-20 20:47:42 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <main className={styles.contentWrapper}>
+      <h1>Hello world</h1>
+    </main>
+  )
+}
+
+export default App
